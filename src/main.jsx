256d78d035c40e1547582b8013bed8bb02cf0074
ronaranjo <rodrigo.naranjo9922@gmail.com>
2024-01-22 23:13:15 -0300

    probando rama personal

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Comentario de prueba
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />

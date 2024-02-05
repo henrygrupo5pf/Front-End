bd3d76d9a9dfad1f568efedb1a75aaa38491d19a
viri <viryubaldomendoza@gmail.com>
2024-02-05 14:02:12 -0600

    autenticacion funcionando

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 export * from './Home.jsx';
 export * from './createForm/ProductForm.jsx';
 export * from './ProductDetail.jsx'
-<<<<<<< HEAD
 export * from './Login.jsx'
-=======
 export * from './Cart/Cart.jsx'; // Asegúrate de que Cart se exporta por defecto en Cart.jsx
->>>>>>> 8c61d35bb1d929d3d427a634bad9df12f50aaa9f
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import { Home, ProductForm, ProductDetail, Login} from "../pages/index";
-=======
-import { Home, ProductForm, ProductDetail} from "../pages/index";
 import Cart from "../pages/Cart/Cart"
 // /src/pages/index.js
-
->>>>>>> 8c61d35bb1d929d3d427a634bad9df12f50aaa9f
 
 const MyRoutes = () => {
   return (
@@ -14,11 +9,8 @@ const MyRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/productForm" element={<ProductForm />} />
       <Route path="/productDetail/:id" element={<ProductDetail/>} />
-<<<<<<< HEAD
       <Route path="/login" element={<Login />} />
-=======
       <Route path="/cart" element={<Cart/>} />
->>>>>>> 8c61d35bb1d929d3d427a634bad9df12f50aaa9f
     </Routes>
   );
 };

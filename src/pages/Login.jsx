dc29c0cfd9274833f787266e08e34dcf15d6a074
viri <viryubaldomendoza@gmail.com>
2024-02-10 13:49:27 -0600

    Login con Gmail y otros botones funcionando

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import appFirebase from '../credenciales';
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { useUserStore } from '../Store/UserStore';
-
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(appFirebase);
 const db = getFirestore(appFirebase);
@@ -12,8 +12,7 @@ const BASE_URL = "https://pf-server-93lj.onrender.com"
 
 
 export const Login = () => {
- 
-  const {setUserAuth, userAuth} = useUserStore()
+  const {setUserAuth} = useUserStore()
 
   const[user, setUser] = useState({
     name: "",
@@ -26,39 +25,33 @@ export const Login = () => {
   const [error, setError] = useState(null);
   const [registrando, setRegistrando] = useState(false); // Estado para manejar el estado de registro/inicio de sesión
   const [usuarioAutenticado, setUsuarioAutenticado] = useState(false); // Estado para indicar si el usuario está autenticado
-  const [isLoading, setIsLoading] = useState(false); // Estado para indicar si la autenticación/registro está en progreso
+  
+  const validateEmail = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6;
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (usuario) => {
       if (usuario) {
         setUsuarioAutenticado(true);
+        setUser(prevUser => ({ ...prevUser, name: usuario.displayName }));
+        setUserAuth(usuario);
       } else {
-        setUserAuth(null)
+        setUserAuth(null);
         setUsuarioAutenticado(false);
       }
     });
     
-    // Limpiar suscripción al desmontar el componente
     return () => unsubscribe();
   }, [setUserAuth]);
 
-  // Método para manejar el inicio de sesión
-  const handleLogin = async () => {
+  const handleAuthentication = async (authFunction, userData) => {
     try {
-      if (!user.email || !user.password) {
-        setError("Por favor, complete todos los campos.");
-        return;
-      }
-      setIsLoading(true); // Iniciar el indicador de carga
-      await signInWithEmailAndPassword(auth, user.email, user.password);
-      // Limpiar los campos de correo electrónico y contraseña después de iniciar sesión
-      setUser({ ...user, email: "", password: "" });
-      setError(null);
-      const userData = {
-        email: user.email,
-        password: user.password
-      }
-
+      await authFunction;
       const response = await fetch(`${BASE_URL}/user/login`, {
         method: 'POST',
         headers: {
@@ -66,89 +59,94 @@ export const Login = () => {
         },
         body: JSON.stringify(userData),
       });
-
-      const userApi = await response.json()
+      const userApi = await response.json();
       setUserAuth(userApi);
-      
     } catch (error) {
       setError(error.message);
-    } finally {
-      setIsLoading(false); // Detener el indicador de carga
     }
   };
-  
+  // Método para manejar el inicio de sesión
+  const handleLogin = async () => {
+    if (!validateEmail(user.email)) {
+      setError("El correo electrónico no es válido");
+      return;
+    }
+    if (!validatePassword(user.password)) {
+      setError("La contraseña debe tener al menos 6 caracteres");
+      return;
+    }
+    handleAuthentication(signInWithEmailAndPassword(auth, user.email, user.password), { email: user.email, password: user.password });
+  };
   // Método para manejar el registro de usuarios
   const handleRegister = async () => {
-    try {
-      if (!user.email || !user.password) {
-        setError("Por favor, complete todos los campos.");
-        return;
-      }
-      if (user.password.length < 6) {
-        setError("La contraseña debe tener al menos 6 caracteres.");
-        return;
-      }
-      setIsLoading(true);
-      await createUserWithEmailAndPassword(auth, user.email, user.password);
-      // Limpiar los campos de correo electrónico y contraseña después de registrarse
-      setUser({ ...user, email: "", password: "" });
-      setError(null);
-      const response = await fetch(`${BASE_URL}/user`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false); // Detener el indicador de carga
+    if (!validateEmail(user.email)) {
+      setError("El correo electrónico no es válido");
+      return;
     }
+    if (!validatePassword(user.password)) {
+      setError("La contraseña debe tener al menos 6 caracteres");
+      return;
+    }
+    handleAuthentication(createUserWithEmailAndPassword(auth, user.email, user.password), user);
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      setUser({ ...user, name: result.user.displayName }); // Actualiza el nombre del usuario en el estado user
+      setUserAuth(result.user);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   // Método para manejar el cierre de sesión
   const handleLogout = async () => {
     try {
       await signOut(auth);
-      // Limpiar los campos de correo electrónico y contraseña
-      setUser({ ...user, email: "", password: "" });
-      setError(null);
+      setUsuarioAutenticado(false);
+      setUserAuth(null);
     } catch (error) {
-      console.error('Error al cerrar sesión:', error);
+      setError('Error al cerrar sesión: ' + error.message);
     }
   };
-
+  // Método para manejar la recuperación de contraseña
+const handleForgotPassword = async () => {
+  if (!validateEmail(user.email)) {
+    setError("Por favor, ingresa un correo electrónico válido para restablecer tu contraseña.");
+    return;
+  }
+  try {
+    await sendPasswordResetEmail(auth, user.email);
+    setError(null);
+    setError("Se ha enviado un correo electrónico con instrucciones para restablecer tu contraseña.");
+  } catch (error) {
+    setError("Ha ocurrido un error al intentar restablecer tu contraseña. Por favor, inténtalo de nuevo más tarde.");
+  }
+};
   const isRegistering = () => {
-    if(registrando){
+    if (registrando) {
       return (
       <div>
-
-      <Input
+        <Input
         type="text"
         placeholder="Nombre"
         value={user.name}
         onChange={(e) => setUser({...user, name: e.target.value})}
       />
-      
-      {/* Validación de entrada para el correo electrónico */}
+
       <Input
         type="email"
         placeholder="Correo Electrónico"
         value={user.email}
         onChange={(e) => setUser({...user, email: e.target.value})}
-        required
       />
 
-      {/* Validación de entrada para la contraseña */}
       <Input
         type="password"
         placeholder="Contraseña"
         value={user.password}
         onChange={(e) => setUser({...user, password: e.target.value})}
-        minLength="6"
-        required
       />
 
       <Input
@@ -171,109 +169,131 @@ export const Login = () => {
         value={user.phoneNumber}
         onChange={(e) => setUser({...user, phoneNumber: e.target.value})}
       />
-
-      
     </div>
-    )
-
-    }else{
-      return(
+    );
+  } else {
+  return(
       <ContainerInput>
-        {/* Validación de entrada para el correo electrónico */}
         <Input
         type="email"
         placeholder="Correo Electrónico"
         value={user.email}
         onChange={(e) => setUser({...user, email: e.target.value})}
-        required
       />
 
-      {/* Validación de entrada para la contraseña */}
       <Input
         type="password"
         placeholder="Contraseña"
         value={user.password}
         onChange={(e) => setUser({...user, password: e.target.value})}
-        minLength="6"
-        required
       />
     </ContainerInput>
       )
     }
   }
-  
+
   return (
     <Container>
       <div className="contentCard">
         <div className="card">
-          <h1 className="h1">Welcome</h1>
+          <WelcomeTitle className="h1">Welcome</WelcomeTitle>
           {usuarioAutenticado ? (
             <>
-              <LoggedInMessage>Bienvenido! Usuario autenticado.</LoggedInMessage>
-              <Button onClick={handleLogout}>Cerrar Sesión</Button>
+              <LoggedInMessage>¡Hola! {user.name}</LoggedInMessage>
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <ReservarButton>Reserva un producto</ReservarButton>
+              </Link>
+              <Link to="/productForm" style={{ textDecoration: 'none' }}>
+                <PublicarButton>Publica un producto</PublicarButton>
+              </Link><LogoutButton onClick={handleLogout}>Cerrar Sesión</LogoutButton>
             </>
           ) : (
             <>
               <Titulo>{registrando ? 'Registrarse' : 'Iniciar Sesión'}</Titulo>
-              
               {isRegistering()}
               {error && <Error>{error}</Error>}
-              {isLoading ? (
-                <LoadingIndicator>Loading...</LoadingIndicator>
-              ) : (
-                <ContainerBtn>
-                  <RegisterButton onClick={registrando ? handleRegister : handleLogin}>{registrando ? 'Registrarse' : 'Iniciar Sesión'}</RegisterButton>
-                  <SwitchButton onClick={() => setRegistrando(!registrando)}>{registrando ? '¿Ya tienes cuenta?' : '¿No tienes cuenta?'}</SwitchButton>
-                </ContainerBtn>
-              )}
+              <ContainerBtn>
+                <RegisterButton onClick={registrando ? handleRegister : handleLogin}>{registrando ? 'Registrarse' : 'Iniciar Sesión'}</RegisterButton>
+                <Button onClick={handleForgotPassword}>¿Olvidaste tu contraseña?</Button>
+                <GoogleLoginButton onClick={handleGoogleLogin}>Iniciar sesión con Google</GoogleLoginButton>
+                <SwitchButton onClick={() => setRegistrando(!registrando)}>{registrando ? '¿Ya tienes cuenta?' : 'Crear cuenta nueva'}</SwitchButton>
+              </ContainerBtn>
             </>
           )}
         </div>
       </div>
     </Container>
   );
-}
+};
 
 export default Login;
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-  .h1 {
-    margin-bottom: 30px;
-  }
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #fff;
+border: none;
+border-radius: 8px;
+box-shadow: 0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1);
+box-sizing: border-box;
+margin: 40px auto 0; 
+padding: 20px 0 28px;
+max-width: 396px;
+width: 90%; 
+justify-content: center;
+text-align: center;
 `;
 
-const Titulo = styled.span``;
+const Titulo = styled.h2`
+text-align: center;
+`;
 
-const ContainerInput = styled.span``;
+const ContainerInput = styled.span`
+  background: #FFFFFF;
+  border: 1px solid #dddfe2;
+  color: #1d2129;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  
+  
+`;   
+const Input = styled.input`
+  width: 85%;
+  border: none;
+  margin-bottom: 15px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  display: block;
+  margin-left: 30px
+  
+  
 
-const Input = styled.input``;
-
+`;   
 const ContainerBtn = styled.div`
   text-align: center;
 `;
 
 const Button = styled.button`
-padding: 10px 15px;
-border: none;
-border-radius: 4px;
-cursor: pointer;
-font-size: 21px;
-margin: 2px;
-transition: background-color 0.3s ease;
+  background-color: white;
+  border: none;
+  border-radius: 6px;
+  font-size: 20px;
+  line-height: 48px;
+  padding: 0 16px;
+  width: 90%;
+  max-width: 332px;
+  margin-bottom: 10px;
+  color: #4caf50;
 
 &:hover {
-  background-color: #45a049;
+  filter: brightness(0.9);
 }
 `;
 
 const RegisterButton = styled(Button)`
-background-color: #4caf50;
+  background-color: #4caf50;
   color: white;
 `;
 
@@ -281,20 +301,35 @@ const SwitchButton = styled(Button)`
 background-color: #1e90ff;
 color: white;
 `;
-const LoadingIndicator = styled.div`
-color: #1e90ff;
-font-size: 18px;
-margin-top: 10px;
+
+const GoogleLoginButton = styled(Button)`
+  background-color: #db4437;
+  color: white;
 `;
 
 const Error = styled.p`
 color: red;
-font-size: 16px;
-margin-top: 8px;
+margin-top: 10px;
 `;
 
 const LoggedInMessage = styled.p`
-color: green;
-font-size: 18px;
-font-weight: bold;
+color: #4caf50;
+margin-top: 10px;
+`;
+const WelcomeTitle = styled.h1`
+  color: #4caf50;
+`;
+const LogoutButton = styled(Button)`
+  background-color: #f44336; // Color de fondo rojo
+  color: white; // Color del texto blanco
+`;
+
+const ReservarButton = styled(Button)`
+  background-color: #ff9800; // Color de fondo naranja
+  color: white; // Color del texto blanco
+`;
+
+const PublicarButton = styled(Button)`
+  background-color: #2196f3; // Color de fondo azul
+  color: white; // Color del texto blanco
 `;

e7cfdac2e2e64743214bcad73471a222e61bc120
FedericoJSosa <131985390+FedericoJSosa@users.noreply.github.com>
2024-02-12 19:52:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );

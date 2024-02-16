197ef025ee71359cfd100e4119e4295d2d15cb27
FedericoJSosa <federico13javiersosa@gmail.com>
2024-02-15 22:49:05 -0300

    Dashboard

---
+++ src/components/moleculas/UserInfo/UserInfo.jsx
@@ -0,0 +1,12 @@
+
+export const UsersInfo = () => {
+
+  return (
+    <div>
+        <div className="id"> ID</div>
+        <div className="title">Nombre</div>
+        <div className="email">Email</div>
+    </div>
+  );
+};
+
--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -1,53 +1,107 @@
 import styled from 'styled-components';
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { UsersInfo } from "../../components/moleculas/UserInfo/UserInfo"
 
 export const DashBoard = () => {
-    return(
-        <Container>
-            <InfoContainer>
-                <SearchBox>
-                <form onSubmit={onSubmit}>
-                    <SearchBar
-                    className="search-bar"
-                    placeholder="Search for something..."
-                    type="search"
-                    // value={searchValue}
-                    // onChange={onChange}
-                    />
-                    <SearchButton type="submit">Search</SearchButton>
-                </form>
-                </SearchBox>
-                <Info>
-                    Aca van todos los users
-                </Info>
-                <ButtonsContainer>
-                    <Button> Crear Usuario</Button>
-                    <Button> Modificar Usuario</Button>
-                </ButtonsContainer>
-            </InfoContainer>
+  const [searchUsers, setSearchUsers] = useState('');
+  
 
-            <InfoContainer>
-                <SearchBox>
-                <form onSubmit={onSubmit}>
-                    <SearchBar
-                    className="search-bar"
-                    placeholder="Search for something..."
-                    type="search"
-                    // value={searchValue}
-                    // onChange={onChange}
-                    />
-                    <SearchButton type="submit">Search</SearchButton>
-                </form>
-                </SearchBox>
-                <Info>
-                    Aca van todos los users
-                </Info>
-                <ButtonsContainer>
-                    <Button> Crear Usuario</Button>
-                    <Button> Modificar Usuario</Button>
-                </ButtonsContainer>
-            </InfoContainer>
-        </Container>
-    )
+ const url= "http://localhost:3001/user"
+
+/*   const fetchUsers = ({ queryKey }) => {
+    const [searchUsers]  = queryKey;
+ 
+    return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Something went wrong. Try again. Código de error: ${response.status}`);
+      }
+
+      return response.json();
+
+    })
+    .then((data) => {
+      if (data.users.length === 0) {
+
+        throw new Error("No users found. Please try searching with different parameter.");
+      }
+      return data;
+    });
+
+  };
+
+  const query = useQuery({
+    queryKey: [userForFetch],
+    queryFn: fetchUsers,
+    
+  }); */
+
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    let userForFetch=searchUsers
+    console.log(userForFetch);
+  };
+ 
+
+  const onChange = (event) => {
+    setSearchUsers(event.target.value);
+  };
+  console.log(searchUsers);
+  return (
+    <Container>
+      <InfoContainer>
+        <SearchBox>
+          <form onSubmit={onSubmit}>
+            <SearchBar
+              className="search-bar"
+              placeholder="Search for something..."
+              type="search"
+              value={searchUsers}
+              onChange={onChange}
+            />
+            <SearchButton type="submit">Search</SearchButton>
+          </form>
+        </SearchBox>
+
+        <UsersInfo>
+
+        </UsersInfo>
+
+        <ButtonsContainer>
+          <Button> Crear Usuario</Button>
+          <Button> Modificar Usuario</Button>
+        </ButtonsContainer>
+      </InfoContainer>
+
+
+      {/* <InfoContainer>
+        <SearchBox>
+          <form onSubmit={onSubmit}>
+            <SearchBar
+              className="search-bar"
+              placeholder="Search for something..."
+              type="search"
+            // value={searchValue}
+            // onChange={onChange}
+            />
+            <SearchButton type="submit">Search</SearchButton>
+          </form>
+        </SearchBox>
+
+        <UsersInfo>
+          Aca van todos los users
+        </UsersInfo>
+
+        <ButtonsContainer>
+          <Button> Crear Usuario</Button>
+          <Button> Modificar Usuario</Button>
+        </ButtonsContainer>
+      </InfoContainer> */}
+    </Container>
+
+  )
 }
 
 export default DashBoard;
@@ -57,6 +111,7 @@ const Container = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    height: 100vh;
 `
 
 const InfoContainer = styled.div`
@@ -101,3 +156,23 @@ transition: background-color 0.3s ease;
   background-color: #45a049; 
   }
 `;
+
+const ButtonsContainer = styled.div`
+padding: 10px 15px;
+border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+const Button = styled.div`
+padding: 10px 15px;
+border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+
+
+  /*  const params = new URLSearchParams();
+    params.append();
+
+    const url = params.toString() === ""
+    ? `http://localhost:3001/user`
+    : `http://localhost:3001/user/${params.toString()}` */
